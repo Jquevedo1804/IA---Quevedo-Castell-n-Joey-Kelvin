6693ed28beb9940822eebb3b1cdbb35f815ec51b
Quevedo Castellón Joey Kelvin <l22170777@culiacan.tecnm.mx>
2026-02-21 19:46:22 -0700

    8puzzle - Quevedo Joey

---
+++ 8puzzle/App.java
@@ -0,0 +1,24 @@
+package 8puzzle;
+
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) {
+        String inicio = "123804765";
+        String meta   = "128437605";
+        List<TBComparativa> resultados = new ArrayList<>();
+
+        System.out.println("--- Primero en Anchura ---");
+        new Arbol(new Nodo(inicio), meta).busquedaAnchura(resultados);
+
+        System.out.println("\n--- Primero en Profundidad ---");
+        new Arbol(new Nodo(inicio), meta).busquedaProfundidad(resultados);
+
+        System.out.println("\n--- Costo Uniforme ---");
+        new Arbol(new Nodo(inicio), meta).costoUniforme(resultados);
+
+        System.out.println("Inicio: " + inicio);
+        System.out.println("Meta: " + meta);
+        TBComparativa.imprimirTabla(resultados);
+    }
+}
